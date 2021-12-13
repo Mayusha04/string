988370c6458a39692698c59d98b2e7517d70c4ea
mayusha <mayushachowdary95@gmail.com>
2021-12-13 21:01:41 +0530

    hello

---
+++ string.js
@@ -0,0 +1,10 @@
+var myStr="JavaScript is the world's most popular programming language";
+console.log(myStr);
+
+//by usig slash
+var myStr="JavaScript is the world's most \"popular programming\" language";
+console.log(myStr);
+
+//by using single quote
+var myStr="JavaScript is the world's most \'popular\' programming language";
+console.log(myStr);
